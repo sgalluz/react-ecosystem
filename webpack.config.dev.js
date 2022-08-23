c30e790bbcc5d1da14ab0cf0ce836ab7079e9019
Stefano Galluzzi <stefano@MacBook-Air-di-Stefano.local>
2022-08-23 16:37:33 +0200

    Improved webpack.config.dev.js

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,47 +1,10 @@
 const path = require('path');
+const config = require('./webpack.config');
 
 module.exports = {
-  entry: './src/index.tsx',
+  ...config,
   mode: 'development',
   devtool: 'inline-source-map',
-  module: {
-    rules: [
-      {
-        test: /\.[jt]s(x?)$/,
-        exclude: /(node_modules)/,
-        use: [
-          {
-            loader: 'babel-loader',
-            options: {
-              presets: ['@babel/env']
-            }
-          },
-          {
-            loader: 'ts-loader',
-            options: {
-              compilerOptions: {
-                noEmit: false
-              }
-            }
-          }
-        ]
-      },
-      {
-        test: /\.s[ac]ss$/i,
-        exclude: /(node_modules)/,
-        use: ['style-loader', 'css-loader', 'sass-loader']
-      }
-    ]
-  },
-  resolve: {
-    extensions: ['*', '.js', '.jsx', '.ts', '.tsx']
-  },
-  output: {
-    path: path.resolve(__dirname, 'dist/'),
-    publicPath: '/dist/',
-    filename: 'bundle.js',
-    clean: true
-  },
   devServer: {
     static: './public',
     port: 3000,
